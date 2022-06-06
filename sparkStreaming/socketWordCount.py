bf2eadc72dd65361a8b4c68bef89ea91773e3c7a
himanshu <iamhimanshu0>
2022-06-06 21:39:05 +0530

    adding spark streamming and text classification

---
+++ sparkStreaming/socketWordCount.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+if __name__ == "__main__":
+    print("Word Count Application....")
+
+    spark = SparkSession.builder.appName("Word Count Application").master("local[*]").getOrCreate()
+
+    stream_df = spark.readStream.format("socket").option("host","localhost").option("port","1100").load()
+
+    stream_df.printSchema()
+
+    # splitting the data in space seperator 
+    stream_words_df = stream_df.select(explode(split("value", " ")).alias("word"))
+
+    # count the word count
+    stream_word_count_df = stream_words_df.groupBy("word").count()
+
+    # writing the query 
+    write_query = stream_word_count_df.writeStream.outputMode("complete").format("console").start()
+    """
+        for output mode we have 3 options
+        - append (by default) :- it will going to append data 
+        - update -: it will going to update only the updated data or new data
+        - complete :- it will going to update complete data
+    """
+
+    write_query.awaitTermination()
+
+    print("Application Completed....")
